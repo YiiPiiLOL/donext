33725e1ffaa1d37cdbdec092ce1997919e139a51
YiiPiiLOL <66677191+YiiPiiLOL@users.noreply.github.com>
2026-09-10 18:15:29 +0200

    Update public files to V2.11.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="donext-v2.10";
+const CACHE="donext-v2.11";
 const ASSETS=["./","./index.html","./manifest.json","./donext-logo-exact.png","./icon-192.png","./icon-512.png","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("donext-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
