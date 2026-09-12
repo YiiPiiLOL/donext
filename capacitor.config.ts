ee2bc9083be3d0fcdfd8fc26630e01ca41416a58
YiiPiiLOL <66677191+YiiPiiLOL@users.noreply.github.com>
2026-09-12 20:01:13 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'se.chiips.donext',
-  appName: 'DoNext',
-  webDir: 'public',
-  server: { androidScheme: 'https' }
-};
-export default config;
